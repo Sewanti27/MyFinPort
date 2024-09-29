0fdd454c2cd8d44a1763e2a4b785723f3071a1a8
Sewanti <85120843+Sewanti@users.noreply.github.com>
2024-09-29 21:04:04 +0530

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,287 @@
+export const navLinks = [
+    {
+      id: 1,
+      name: 'Home',
+      href: '#home',
+    },
+    {
+      id: 2,
+      name: 'About',
+      href: '#about',
+    },
+    {
+      id: 3,
+      name: 'Work',
+      href: '#work',
+    },
+    {
+      id: 4,
+      name: 'Contact',
+      href: '#contact',
+    },
+  ];
+  
+  export const clientReviews = [
+    {
+      id: 1,
+      name: 'Emily Johnson',
+      position: 'Marketing Director at GreenLeaf',
+      img: 'assets/review1.png',
+      review:
+        'Working with Sewanti was a fantastic experience. She transformed our outdated website into a modern, user-friendly platform. Her attention to detail and commitment to quality are unmatched. Highly recommend her for any web dev projects.',
+    },
+    {
+      id: 2,
+      name: 'Mark Rogers',
+      position: 'Founder of TechGear Shop',
+      img: 'assets/review2.png',
+      review:
+        'Sewanti’s expertise in web development is truly impressive. She delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. She’s a true professional! Fantastic work.',
+    },
+    {
+      id: 3,
+      name: 'John Dohsas',
+      position: 'Project Manager at UrbanTech ',
+      img: 'assets/review3.png',
+      review:
+        'I can’t say enough good things about Sewanti. She was able to take our complex project requirements and turn them into a seamless, functional website. Her problem-solving abilities are outstanding.',
+    },
+    {
+      id: 4,
+      name: 'Ether Smith',
+      position: 'CEO of BrightStar Enterprises',
+      img: 'assets/review4.png',
+      review:
+        'Sewanti was a pleasure to work with. She understood our requirements perfectly and delivered a website that exceeded our expectations. Her skills in both frontend backend dev are top-notch.',
+    },
+  ];
+  
+  export const myProjects = [
+    {
+      title: 'Brainwave- AI Chat App',
+      desc: 'Brainwave website is a sleek, responsive OpenAI chat app featuring parallax effects and Bento grid layouts, built with React and Vite. The design is visually dynamic, using Tailwind CSS to create a modern, intuitive interface. The site is interactive and responsive, providing an engaging user experience as it seamlessly integrates conversational AI functionality with stunning visual elements.',
+      subdesc:
+        'Brainwave combines the power of AI with cutting-edge web design. Users can interact with an OpenAI chat system while enjoying smooth parallax scrolling and beautifully arranged Bento grids, offering both functionality and aesthetic appeal. The responsive layout ensures a seamless experience across devices, while the minimalist design emphasizes clarity and ease of use.',
+      href: 'https://sewanti27.github.io/brainwave_site_deploy/',
+      texture: '/textures/project/project1.mp4',
+      logo: '/assets/brainwave-symbol.svg',
+      logoStyle: {
+        backgroundColor: '#2A1816',
+        border: '0.2px solid #36201D',
+        boxShadow: '0px 0px 60px 0px #AA3C304D',
+      },
+      spotlight: '/assets/spotlight1.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: 'E-commerce Website',
+      desc: 'MyPoojaBox is an elegant and user-friendly e-commerce platform designed to showcase and sell spiritual and devotional products. Built with React and Vite, the website offers a clean, organized interface for easy navigation and product discovery',
+      subdesc:
+        'Explore a curated selection of spiritual items in a beautifully designed online store. With a minimalist layout and smooth user experience, MyPoojaBox allows visitors to browse and shop with ease, ensuring a seamless and serene shopping journey.',
+      href: 'https://sewanti27.github.io/MyPoojaBox/',
+      texture: '/textures/project/project5.mp4',
+      logo: '/assets/PoojaLogo.png',
+      logoStyle: {
+        backgroundColor: '#60f5a1',
+        background:
+          'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+        border: '0.2px solid rgba(208, 213, 221, 1)',
+        boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+      },
+      spotlight: '/assets/spotlight3.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: '3D Portfolio Website',
+      desc: 'This 3D portfolio is an immersive, interactive experience showcasing creative work in a visually captivating format. Built using React, Vite, and Three.js, the portfolio features dynamic 3D models and smooth navigation, offering a unique way to present projects.',
+      subdesc:
+        'Explore a 3D island with interactive elements that highlight various projects in a creative and engaging way. With its seamless animations, responsive design, and innovative use of Three.js and React, this portfolio offers a visually stunning experience for showcasing your work.',
+      href: 'https://sewanti27.github.io/3DPortfolio_Sewanti/',
+      texture: '/textures/project/project4.mp4',
+      logo: '/assets/project-logo4.png',
+      logoStyle: {
+        backgroundColor: '#0E1F38',
+        border: '0.2px solid #0E2D58',
+        boxShadow: '0px 0px 60px 0px #2F67B64D',
+      },
+      spotlight: '/assets/spotlight4.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: 'Cavial',
+      desc: 'Cavial is a unique platform designed to help cat owners find the perfect partner for their feline companions. Built with React and Vite, the site offers a user-friendly interface and a fun, engaging experience for pet matchmaking.',
+      subdesc:
+        'Discover the ideal match for your cat on Cavial, a platform tailored for pet lovers seeking companionship for their furry friends. The site combines sleek design with easy navigation, making it simple and enjoyable to browse potential partners for your beloved cat.',
+      href: 'https://sewanti27.github.io/Cavial/',
+      texture: '/textures/project/project3.mp4',
+      logo: '/assets/Catlogo.png',
+      logoStyle: {
+        backgroundColor: '#1C1A43',
+        border: '0.2px solid #252262',
+        boxShadow: '0px 0px 60px 0px #635BFF4D',
+      },
+      spotlight: '/assets/spotlight5.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: 'Apple Clone Website',
+      desc: 'The Apple Clone website is a visually striking and highly responsive replica of Apple homepage, showcasing a sleek design that mirrors the aesthetic of the original. Built with React, Vite, and GSAP for smooth animations, it provides a fluid and interactive user experience.',
+      subdesc:
+        'Experience a dynamic and responsive Apple-inspired interface with captivating animations. The site features smooth transitions and minimalistic design, offering a seamless browsing experience that echoes Apples high-quality, modern appeal.',
+      href: 'https://sewanti27.github.io/Apple_Clone_Website/',
+      texture: '/textures/project/project2.mp4',
+      logo: '/assets/apple.svg',
+      logoStyle: {
+        backgroundColor: '#13202F',
+        border: '0.2px solid #17293E',
+        boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      },
+      spotlight: '/assets/spotlight2.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+  ];
+  
+  export const calculateSizes = (isSmall, isMobile, isTablet) => {
+    return {
+      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+      deskPosition: isMobile ? [0.5, -5.5, 0] : [0.25, -7.25, 0],
+      cubePosition: isSmall ? [4, -5, 0] : isMobile ? [7, -5, 0] : isTablet ? [7.5, -5, 0] : [9, -5.5, 0],
+      reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [7, 4, 0] : [12, 3, 0],
+      ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-12, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+      targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-14, -7, -10] : [-13, -13, -10],
+    };
+  };
+  export const workExperiences = [
+    {
+      id: 1,
+      name: 'Honeywell Technology Solutions',
+      pos: 'Sustainability COE Intern',
+      duration: 'January, 2024 - July, 2024',
+      title: "Conducted Life Cycle Assessments (LCA) and delivered presentations on sustainability standards (RoHS, ISO,ECD, IEC, WEEE) in the HBT team. Analyzed vehicle technologies for environmental impact using literature benchmarking and evaluated jet fuel production from ethanol via SimaPro. Developed skills in teamwork, collaboration, and application of ESG principles in sustainability projects",
+      icon: '/assets/honeywell.jpg',
+      animation: 'victory',
+    },
+    {
+      id: 2,
+      name: 'MavenMagnet',
+      pos: 'ReactJS Developer Intern',
+      duration: 'July 2022- November 2022',
+      title: "Collaborated with a talented team of developers and designers to develop a dashboard using ReactJS, which allowed users to visualize and analyze large sets of data in an intuitive and interactive manner. Leveraged my proficiency in JavaScript and JQuery to enhance the dashboard's functionality.Designed and developed graphs using the different libraries present in ReactJS",
+      icon: '/assets/mavenmagnet.jpeg',
+      animation: 'clapping',
+    },
+    {
+      id: 3,
+      name: 'SHMT',
+      pos: 'Junior Web Developer',
+      duration: 'January,2022 - June-2022',
+      title: "Worked extensively with HTML, CSS, and ReactJS in front-end development. Collaborated on developing responsive websites with pixel-perfect designs and optimal user interactions. Contributed to implementing responsive design techniques by using CSS media queries and Bootstrap's responsive grid system to create flexible layouts for consistent experiences across devices.",
+      icon: '/assets/notion.svg',
+      animation: 'salute',
+    },
+  ];
