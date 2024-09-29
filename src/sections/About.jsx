9d4f061022c85653b696c37b304d7e93d14a694f
Sewanti Lahiri <87894935+Sewanti27@users.noreply.github.com>
2024-09-29 21:58:16 +0530

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
     <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
     <div className="col-span-1 xl:row-span-3">
     <div className="grid-container">
-    <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain"/>
+    <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain"/>
     <div>
         <p className="grid-headtext">Hi, I'm Sewanti</p>
         <p className="grid-subtext">With 2 years of experience, I have honed my skills in frontend and backend development, with a focus on animated 3D websites</p>
@@ -26,7 +26,7 @@ const About = () => {
     </div>
     <div className="col-span-1 xl:row-span-3">
     <div className="grid-container">
-    <img src="/assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain"/>
+    <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain"/>
     <div>
         <p className="grid-headtext">Tech Stack</p>
         <p className="grid-subtext">I specialize in Javascript with a focus on React and Next.js ecosystems</p>
@@ -58,7 +58,7 @@ const About = () => {
     </div>
     <div className="xl:col-span-2 xl:row-span-3">
     <div className="grid-container">
-    <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain"/>
+    <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain"/>
     <div>
         <p className="grid-headtext"> My Passion for Coding</p>
         <p className="grid-subtext"> I love solving problems and finding solutions through code. Coding isn't just my profession- it is my passion</p>
@@ -68,11 +68,11 @@ const About = () => {
     </div>
     <div className="xl:col-span-1 xl:row-span-2">
     <div className="grid-container">
-    <img src="/assets/grid4.png" alt="grid-4" className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"/>
+    <img src="assets/grid4.png" alt="grid-4" className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"/>
     <div className="space-y-2">
         <p className="grid-subtext text-center">Contact Me</p>
         <div className="copy-container" onClick={handleCopy}>
-            <img src={hasCopied ? "/assets/tick.svg" : "/assets/copy.svg"} alt="copy"/>
+            <img src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"} alt="copy"/>
             <p className="lg:text-xl md:text-xl font-medium text-gray_gradient text-white">lahirisewanti2002@gmail.com</p>
         </div>
     </div>
